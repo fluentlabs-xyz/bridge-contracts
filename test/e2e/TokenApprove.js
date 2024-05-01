eb13b11da30c19ceafeb7bb2eb98266debc96886
Dmitry Savonin <nwtsoas@gmail.com>
2024-05-01 12:54:32 +0400

    fixed token approve test

--- test/e2e/TokenApprove.js
+++ test/e2e/TokenApprove.js
@@ -2,22 +2,28 @@ const {expect} = require("chai");
 const {ethers} = require("hardhat");
 const consts = require(`../../consts`);
 
-describe("Contract deployment and interaction", () => {
+describe("Contract deployment and interaction 2", () => {
     let l1Token;
     // let l1Url = `http://${consts.EVM_HOST}:${consts.EVM_NODE_PORT}/`;
     let l1Url = `http://${consts.FLUENT_HOST}:${consts.FLUENT_NODE_PORT}/`;
 
+    let providerL1 = new ethers.providers.JsonRpcProvider(l1Url); // Replace with your node's RPC URL
+    let wallet = ethers.Wallet.fromMnemonic("test test test test test test test test test test test junk")
+    let signerL1 = wallet.connect(providerL1)
+
+    let fromAccount = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+    let toAccount = '0x0000000000000000000000000000000000000000';
+
+    const accounts = [fromAccount, toAccount];
+
     before(async () => {
-        const accounts = await ethers.getSigners();
-
-        let signerL1 = accounts[0]
-
         const Token = await ethers.getContractFactory("MockERC20Token");
         l1Token = await Token.connect(signerL1).deploy(
             "Mock Token",
             "TKN",
             ethers.utils.parseEther("1000000"),
-            accounts[0].address, {
+            fromAccount,
+            {
                 gasLimit: 2000000,
             }
         );
@@ -26,18 +32,17 @@ describe("Contract deployment and interaction", () => {
     });
 
     it("Approve test", async () => {
-        let provider = new ethers.providers.JsonRpcProvider(l1Url);
-        let accounts = await provider.listAccounts();
-
         let allowance = await l1Token.allowance(accounts[0], accounts[1]);
         console.log(`account0-account1 allowance: ${allowance}`)
 
         expect(allowance.toString()).to.equal("0");
 
         const approve_tx = await l1Token.approve(accounts[1], 100, {
+            from: accounts[0],
             gasLimit: 2000000,
         });
         let r = await approve_tx.wait();
+        console.log(r);
         allowance = await l1Token.allowance(accounts[0], accounts[1]);
         console.log(`account0-account1 allowance: ${allowance}`)
 
