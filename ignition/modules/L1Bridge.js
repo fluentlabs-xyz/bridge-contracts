2764680df0da34cde9582a5d8ac9e468194c26de
Bogdan Shinkarenko <shinbogdan@gmail.com>
2024-06-11 01:00:40 +1000

    feat: updated l1 ignition scripts

--- ignition/modules/L1Bridge.js
+++ ignition/modules/L1Bridge.js
@@ -1,23 +1,71 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const {ethers} = require("ethers");
 
-module.exports = buildModule("L1Bridge", (m) => {
+const mockTokenModule = buildModule("mockToken", (m) => {
+    let owner = m.getAccount(0)
+    
+    const mockToken = m.contract("MockERC20Token",  [
+        "Mock Token", "TKN", ethers.parseEther("1000000"), owner
+    ]);
+
+    return {mockToken}
+});
+
+const peggedTokenModule = buildModule("peggedToken", (m) => {
+    const peggedToken = m.contract("ERC20PeggedToken", []);
+
+    return {peggedToken}
+});
+
+const rollupModule = buildModule("Rollup", (m) => {
+	const rollup = m.contract("Rollup", [])
+
+	return {rollup};
+});
+
+const bridgeModule = buildModule("Bridge", (m) => {
     let owner = m.getAccount(0)
 
-    const mockToken = m.contract("MockERC20Token",
-    [
-            "Mock Token", "TKN", ethers.parseEther("1000000"), owner
-        ]);
-    const peggedToken = m.contract("ERC20PeggedToken", []);
-    const rollup= m.contract("Rollup", []);
-
-    const bridge = m.contract("Bridge", [owner, rollup])
-    const tokenFactory = m.contract("ERC20TokenFactory", [peggedToken])
-    const erc20Gateway = m.contract("ERC20Gateway", [bridge, tokenFactory])
-
-    m.call(tokenFactory, "transferOwnership", [erc20Gateway])
+    const {rollup} = m.useModule(rollupModule);
+	const bridge = m.contract("Bridge", [owner, rollup])
 
     m.call(rollup, "setBridge", [bridge])
 
-    return { bridge, erc20Gateway, mockToken };
+	return {bridge}
+});
+const tokenFactoryModule = buildModule("ERC20TokenFactory", (m) => {
+    const {peggedToken} = m.useModule(peggedTokenModule)
+
+	const tokenFactory = m.contract("ERC20TokenFactory", [peggedToken])
+
+	return {tokenFactory}
+});
+const erc20GatewayModule = buildModule("ERC20Gateway", (m) => {
+    const {bridge} = m.useModule(bridgeModule)
+
+    const {tokenFactory} = m.useModule(tokenFactoryModule)
+
+	const erc20Gateway = m.contract("ERC20Gateway", [bridge, tokenFactory])
+
+    m.call(tokenFactory, "transferOwnership", [erc20Gateway])
+
+	return {erc20Gateway}
+});
+
+
+module.exports = buildModule("L1Bridge", (m) => {
+
+    const {mockToken} = m.useModule(mockTokenModule)
+
+    const {peggedToken} = m.useModule(peggedTokenModule)
+
+    const {rollup} = m.useModule(rollupModule);
+
+    const {bridge} = m.useModule(bridgeModule)
+
+    const {tokenFactory} = m.useModule(tokenFactoryModule)
+
+    const {erc20Gateway} = m.useModule(erc20GatewayModule)
+
+    return { bridge, erc20Gateway, mockToken, rollup, peggedToken, tokenFactory };
 });
--- ignition/modules/L1Restaker.js
+++ ignition/modules/L1Restaker.js
@@ -2,51 +2,140 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const {ethers} = require("ethers");
 const L1Bridge = require("./L1Bridge")
 
-
-module.exports = buildModule("L1Restaker", (m) => {
-
-    const module = m.useModule(L1Bridge)
-
-    let bridge = module.bridge
-
+const protocolConfigModule = buildModule("ProtocolConfig", (m) => {
     let owner = m.getAccount(0)
-
     const protocolConfig = m.contract("ProtocolConfig", [owner, owner, owner]);
-    const ratioFeed = m.contract("RatioFeed", [protocolConfig, "40000"]);
+
+    return {protocolConfig}
+});
+
+const ratioFeedModule = buildModule("RatioFeed", (m) => {
+    const { protocolConfig} = m.useModule(protocolConfigModule);
+
+	const ratioFeed = m.contract("RatioFeed", [protocolConfig, "40000"])
 
     m.call(protocolConfig, "setRatioFeed", [ratioFeed]);
 
-    const liquidityToken= m.contract("LiquidityToken", [protocolConfig, 'Liquidity Token', 'lETH',]);
+	return {ratioFeed }
+});
+
+const liquidityTokenModule = buildModule("LiquidityToken", (m) => {
+    const { protocolConfig} = m.useModule(protocolConfigModule);
+
+	const liquidityToken= m.contract("LiquidityToken", [protocolConfig, 'Liquidity Token', 'lETH',])
 
     m.call(ratioFeed, "updateRatio", [liquidityToken, "1000000000000000000"])
 
     m.call(protocolConfig, "setLiquidityToken", [liquidityToken]);
 
-    const restakingPool= m.contract("RestakingPool", [protocolConfig, '200000', '200000000000000000000']);
+	return {liquidityToken}
+});
+
+const restakingPoolModule = buildModule("RestakingPool", (m) => {
+    const { protocolConfig} = m.useModule(protocolConfigModule)
+
+	const restakingPool= m.contract("RestakingPool", [protocolConfig, '200000', '200000000000000000000'])
 
     m.call(protocolConfig, "setRestakingPool", [restakingPool]);
 
-    const feeCollector= m.contract("FeeCollector", [protocolConfig, '1500']);
+    m.call(restakingPool, "addRestaker", ["RESTAKER_PROVIDER"])
 
-    const peggedToken= m.contract("ERC20PeggedToken", []);
+	return {restakingPool}
+});
 
-    const tokenFactory= m.contract("ERC20TokenFactory", [peggedToken]);
+const feeCollectorModule = buildModule("FeeCollector", (m) => {
+    const { protocolConfig} = m.useModule(protocolConfigModule);
 
-    const restakerGateway= m.contract("RestakerGateway", [bridge, restakingPool, tokenFactory]);
+	const feeCollector= m.contract("FeeCollector", [protocolConfig, '1500'])
+	return {feeCollector}
+});
 
-    const restakerFacets= m.contract("RestakerFacets", [owner, "0x30770d7E3e71112d7A6b7259542D1f680a70e315", "0xdfB5f6CE42aAA7830E94ECFCcAd411beF4d4D5b6"]);
+const peggedTokenModule = buildModule("ERC20PeggedToken", (m) => {
+	const peggedToken= m.contract("ERC20PeggedToken", [])
+	return {peggedToken}
+});
 
-    const restaker= m.contract("Restaker", []);
+const tokenFactoryModule = buildModule("ERC20TokenFactory", (m) => {
+    const {peggedToken} = m.useModule(peggedTokenModule);
 
-    const upgradeableBeacon= m.contract("UpgradeableBeacon", [restaker, owner]);
+	const tokenFactory= m.contract("ERC20TokenFactory", [peggedToken])
+	return {tokenFactory}
+});
 
-    const restakerDeployer= m.contract("RestakerDeployer", [upgradeableBeacon, restakerFacets]);
+const restakerGatewayModule = buildModule("RestakerGateway", (m) => {
+    const module = m.useModule(L1Bridge)
 
-    m.call(protocolConfig, "setRestakerDeployer", [restakerDeployer])
+    let bridge = module.bridge
+
+    const {tokenFactory} = m.useModule(tokenFactoryModule);
+    const {restakingPool} = m.useModule(restakingPoolModule);
+
+
+	const restakerGateway= m.contract("RestakerGateway", [bridge, restakingPool, tokenFactory])
 
     m.call(tokenFactory, "transferOwnership", [restakerGateway])
 
-    m.call(restakingPool, "addRestaker", ["RESTAKER_PROVIDER"])
+	return {restakerGateway}
+});
 
-    return { restakingPool, restakerGateway };
+const restakerFacetsModule = buildModule("RestakerFacets", (m) => {
+    let owner = m.getAccount(0)
+
+	const restakerFacets= m.contract("RestakerFacets", [owner, "0x30770d7E3e71112d7A6b7259542D1f680a70e315", "0xdfB5f6CE42aAA7830E94ECFCcAd411beF4d4D5b6"])
+	return {restakerFacets}
+});
+
+const restakerModule = buildModule("Restaker", (m) => {
+	const restaker= m.contract("Restaker", [])
+	return {restaker}
+});
+
+const upgradeableBeaconModule = buildModule("UpgradeableBeacon", (m) => {
+    let owner = m.getAccount(0)
+    const {restaker} = m.useModule(restakerModule);
+
+	const upgradeableBeacon= m.contract("UpgradeableBeacon", [restaker, owner])
+	return {upgradeableBeacon}
+});
+
+const restakerDeployerModule = buildModule("RestakerDeployer", (m) => {
+    const {upgradeableBeacon} = m.useModule(upgradeableBeaconModule);
+    const {restakerFacets} = m.useModule(restakerFacetsModule);
+    const { protocolConfig} = m.useModule(protocolConfigModule);
+
+	const restakerDeployer= m.contract("RestakerDeployer", [upgradeableBeacon, restakerFacets])
+
+    m.call(protocolConfig, "setRestakerDeployer", [restakerDeployer])
+
+	return {restakerDeployer}
+});
+
+
+module.exports = buildModule("L1Restaker", (m) => {
+
+    const { protocolConfig} = m.useModule(protocolConfigModule);
+
+    const {ratioFeed } = m.useModule(ratioFeedModule);
+
+    const {liquidityToken} = m.useModule(liquidityTokenModule);
+
+    const {restakingPool} = m.useModule(restakingPoolModule);
+
+    const {feeCollector} = m.useModule(feeCollectorModule);
+
+    const {peggedToken} = m.useModule(peggedTokenModule);
+
+    const {tokenFactory} = m.useModule(tokenFactoryModule);
+
+    const {restakerGateway} = m.useModule(restakerGatewayModule);
+
+    const {restakerFacets} = m.useModule(restakerFacetsModule);
+
+    const {restaker} = m.useModule(restakerModule);
+
+    const {upgradeableBeacon} = m.useModule(upgradeableBeaconModule);
+
+    const {restakerDeployer} = m.useModule(restakerDeployerModule);
+
+    return { restakingPool, restakerGateway, protocolConfig, ratioFeed, liquidityToken, feeCollector, peggedToken, tokenFactory, restakerFacets, restaker, upgradeableBeacon, restakerDeployer };
 });
